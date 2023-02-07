0617e66a2453f6c06e90fefb629040cbd1b587a8
Larkinov <buyantolarkinov@gmail.com>
2023-02-07 16:23:04 +0700

    Делаю верстку

--- src/App.js
+++ src/App.js
@@ -21,7 +21,62 @@ function App() {
       </header>
       <div className="content">
         <h1>Все товары</h1>
-        ....
+        
+        <div className="forest">
+          <div className="card">
+            <img src="/img/forest/tree-one.svg" alt="tree" width={133} height={112}/>
+            <h5>Лучшая елочка на районе</h5>
+            <div className="cardButton">
+              <div className="cardButtonPrice">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11}/>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img src="/img/forest/tree-two.svg" alt="tree" width={133} height={112}/>
+            <h5>Лучшая елочка на районе</h5>
+            <div className="cardButton">
+              <div className="cardButtonPrice">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11}/>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img src="/img/forest/tree-three.svg" alt="tree" width={133} height={112}/>
+            <h5>Лучшая елочка на районе</h5>
+            <div className="cardButton">
+              <div className="cardButtonPrice">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11}/>
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img src="/img/forest/tree-five.svg" alt="tree" width={133} height={112}/>
+            <h5>Лучшая елочка на районе</h5>
+            <div className="cardButton">
+              <div className="cardButtonPrice">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img src="/img/plus.svg" alt="Plus" width={11} height={11}/>
+              </button>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
